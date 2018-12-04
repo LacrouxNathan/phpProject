d97da9c63d54fb3778264b7de3a34469ce352294
Lacroux Nathan <rlx@RLXLINUX.home>
2018-12-04 10:27:29 +0100

    Update du css

--- html/index.php
+++ html/index.php
@@ -13,23 +13,26 @@ $listeJ[] = new Joueur("Coquet", "Marie", "24/09/1999", 155, 90);
 	</head>
 
 	<body>
-		<table>
-			<tr>
-				<td>Nom</td>
-				<td>Prenom</td>
-				<td>Poste</td>
-				<td>Statut</td>
-			</tr>
-			<?php
-			foreach ($listeJ as $joueur) {
-				echo "<tr>
-						<td>".$joueur->getNom()."</td>
-						<td>".$joueur->getPrenom()."</td>
-						<td>".$joueur->getPoste()."</td>
-						<td>".$joueur->getStatut()."</td>
-					  </tr>";
-			}
-			?>
-		</table>
+		<?php include("menu.php");?>
+		<div id="tableau">
+			<table>
+				<tr>
+					<td>Nom</td>
+					<td>Prenom</td>
+					<td>Poste</td>
+					<td>Statut</td>
+				</tr>
+				<?php
+				foreach ($listeJ as $joueur) {
+					echo "<tr>
+							<td>".$joueur->getNom()."</td>
+							<td>".$joueur->getPrenom()."</td>
+							<td>".$joueur->getPoste()."</td>
+							<td>".$joueur->getStatut()."</td>
+						</tr>";
+				}
+				?>
+			</table>
+		</div>
 	</body>
 </html>
